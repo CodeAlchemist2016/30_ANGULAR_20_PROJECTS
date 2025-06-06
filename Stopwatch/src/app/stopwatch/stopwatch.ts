a5e29f4a1ecfda2cd9926ac9bae4f3fdb8e3f7ea
CodeAlchemist2016 <evgenij55555@gmail.com>
2025-06-06 15:08:12 +0500

    Stopwatch app is completed

---
+++ Stopwatch/src/app/stopwatch/stopwatch.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import {DecimalPipe} from '@angular/common';
+
+@Component({
+             selector: 'stopwatch',
+             imports: [DecimalPipe],
+             templateUrl: './stopwatch.html',
+             styleUrl: './stopwatch.scss'
+           }) export class Stopwatch {
+  elapsedTime = 0;
+  isRunning = false;
+  intervalRef: any;
+
+  startStop() {
+    this.isRunning ? this.stop() : this.start();
+  }
+
+  private start() {
+    this.isRunning =
+      true;
+    this.intervalRef =
+      setInterval(() => {
+                    this.elapsedTime +=
+                      0.1;
+                  },
+                  100);
+    console.log('Stopwatch started');
+  }
+
+  private stop() {
+    this.isRunning = false;
+    clearInterval(this.intervalRef);
+    console.log('Stopwatch stopped');
+  }
+
+  reset() {
+    this.isRunning = false;
+    clearInterval(this.intervalRef);
+    this.elapsedTime = 0;
+    console.log('Stopwatch reset');
+  }
+}
